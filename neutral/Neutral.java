5b579b190597d7512914364e4e03fb9067a47368
Allen468 <allenblessingr@gmail.com>
2025-03-13 02:26:35 -0400

    this is my meutral program

---
+++ neutral/Neutral.java
@@ -0,0 +1,19 @@
+public class Neutral{
+    public static void main(String args[]){
+        int number=20;
+        if(number<0){
+            System.out.println("It is negative number");
+            
+
+        }
+        else if(number==0){
+            System.out.println("it is neutral");
+        }
+        else{
+            System.out.println("It is positive number");
+        
+        }
+    }
+
+}     
+    
