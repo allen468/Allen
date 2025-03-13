0fe3f1637d94a6ae69083c1407a96e6d5222db0e
Allen468 <allenblessingr@gmail.com>
2025-03-13 02:09:28 -0400

    this is my positivenegative program

---
+++ positive/positivenegative.java
@@ -0,0 +1,15 @@
+public class positivenegative{
+    public static void main(String args[]){
+        int number=20;
+        if(number<0){
+            System.out.println("It is negative number");
+
+        }
+        else{
+            System.out.println("It is positive number");
+        
+        }
+    }
+
+}     
+    
