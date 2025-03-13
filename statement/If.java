b64680651f85e3bad943476975098898b624dc27
Allen468 <allenblessingr@gmail.com>
2025-03-13 01:30:10 -0400

    this is If statement

---
+++ statement/If.java
@@ -0,0 +1,14 @@
+public class If{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("my name is allenblessing48");
+
+        }
+        if(false){
+            System.out.println("my name is allen");
+        
+        }
+    }
+
+}     
+    
