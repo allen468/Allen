693df297abefcaa3040139bd1f467abce5d28ff9
Allen468 <allenblessingr@gmail.com>
2025-03-13 01:38:35 -0400

    this is Ifelse statement operator

---
+++ statement/Ifelse.java
@@ -0,0 +1,13 @@
+public class Ifelse{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("my name is allenblessing48");
+
+        }
+        else{
+            System.out.println("my name is allen");
+        
+        }
+    }
+
+}     
