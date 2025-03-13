08d4d15617ab1300c1b0883c8b1a56ed9283a935
Allen468 <allenblessingr@gmail.com>
2025-03-13 02:01:37 -0400

    this is my Majorminor program

---
+++ major/Majorminor.java
@@ -0,0 +1,15 @@
+public class Majorminor{
+    public static void main(String args[]){
+        int age=20;
+        if(age<18){
+            System.out.println("you are minor");
+
+        }
+        else{
+            System.out.println("you are major");
+        
+        }
+    }
+
+}     
+    
