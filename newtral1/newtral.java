0e952f4d6521563461caa6939ff93f360ad208f4
Allen468 <allenblessingr@gmail.com>
2025-03-14 00:20:56 -0400

    this is my newtral1 program

---
+++ newtral1/newtral.java
@@ -0,0 +1,23 @@
+public class newtral{
+    public static void main(String args[]){
+        int number=20;
+        if(number<0){
+            System.out.println("It is negative number");
+            
+
+        }
+        else if(number==0){
+            System.out.println("it is neutral");
+
+
+
+            
+        }
+        else{
+            System.out.println("It is positive number");
+        
+        }
+    }
+
+}     
+    
