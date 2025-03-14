11548716a609be841fa3ea20a0ebc1c9e670a030
Allen468 <allenblessingr@gmail.com>
2025-03-14 05:25:14 -0400

    i

---
+++ if/i.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public static void main(String args[]){
+    String name = "hello world";
+
+    System.out.println(name.touperCase());
+    System.out.println(name.tolowerCase());
