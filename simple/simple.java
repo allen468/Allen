c157e036023537fa4a6a3903e3e12536b1ec7ae6
Allen468 <allenblessingr@gmail.com>
2025-03-14 04:58:23 -0400

    this is my simple program

---
+++ simple/simple.java
@@ -0,0 +1,16 @@
+class Child extends parent{
+    void display(){
+        System.out.println("hello");
+    }
+}
+class Child extends parent{
+    void display(){
+        Suystem.out.println("home");
+    }
+}
+public class Simple{
+    public static void main(String args[]){
+        Child obj=new child();
+        obj.display();
+    }
+}
