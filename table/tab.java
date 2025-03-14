d028a7f7722b4b47a53f5d58d9b4743905b11935
Allen468 <allenblessingr@gmail.com>
2025-03-14 05:23:38 -0400

    'tab

---
+++ table/tab.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Scanner{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = input.nextLine();
+        System.out.println(name);
